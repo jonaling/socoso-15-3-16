4469a24f8ae4ee11ec3dc4d6bcc0e116c6d05b6c
jonaling <jonaling00@gmail.com>
2016-03-14 13:57:35 +1300

    First commir

---
+++ MatchBatch1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MatchBatch1.MvcApplication" Language="C#" %>
---
+++ MatchBatch1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MatchBatch1.Startup))]
+namespace MatchBatch1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
